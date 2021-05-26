da0da6bbb6199701d8b57b6edf9f25f9f1bee11c
Ioan Andrei <ivan.andrei93@mail.ru>
2021-05-26 21:12:41 +0200

    Finnished port

---
+++ main.py
@@ -0,0 +1,16 @@
+import socket
+
+target_addr = input("Please type in the target IP address: ")
+port_range = input("Please type in the port range, ex. 0 - 200")
+
+lowpoint = port_range.split("-" [0])
+highpoint = port_range.split("-" [1])
+
+for port in range(lowpoint, highpoint):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = s.connect_ex(target_addr, port)
+    if(result == 0):
+        print("*** Port,",port, " - is open.***")
+    else:
+        print("*** Port,",port, " - is closed.***")    
+s.close()
