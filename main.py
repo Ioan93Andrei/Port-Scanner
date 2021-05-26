aa04f750d20ae1eb3c0b48ba61ba81a61c112280
Ioan Andrei <ivan.andrei93@mail.ru>
2021-05-27 01:04:56 +0200

    fixed some strings to make them format properly.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import socket
 
 target_addr = input("Please type in the target IP address: ")
-port_range = input("Please type in the port range, ex. 0 - 200")
+port_range = input("Please type in the port range, ex. 0 - 200: ")
 
 lowpoint = int(port_range.split("-")[0])
 highpoint = int(port_range.split("-")[1])
@@ -10,7 +10,7 @@ for port in range(lowpoint, highpoint):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     result = s.connect_ex((target_addr, port))
     if(result == 0):
-        print("*** Port,",port, " - is open.***")
+        print("*** Port",port," - is open.***")
     else:
-        print("*** Port,",port, " - is closed.***")    
+        print("*** Port",port," - is closed.***")    
 s.close()
