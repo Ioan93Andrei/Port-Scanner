72fac0495e99a6b7281d867e989d0699885e9648
Ioan Andrei <ivan.andrei93@mail.ru>
2021-05-26 21:35:28 +0200

    corrected code

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import socket
 target_addr = input("Please type in the target IP address: ")
 port_range = input("Please type in the port range, ex. 0 - 200")
 
-lowpoint = port_range.split("-" [0])
-highpoint = port_range.split("-" [1])
+lowpoint = int(port_range.split("-")[0])
+highpoint = int(port_range.split("-")[1])
 
 for port in range(lowpoint, highpoint):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    result = s.connect_ex(target_addr, port)
+    result = s.connect_ex((target_addr, port))
     if(result == 0):
         print("*** Port,",port, " - is open.***")
     else:
